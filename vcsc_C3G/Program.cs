5e5d104474be2ff45c5876e7e05e0a685309b25c
SDEBEUL <SDEBEUL@volvocars.com>
2015-04-16 16:10:13 +0200

    end of workday ... export to excel oke

--- vcsc_C3G/Program.cs
+++ vcsc_C3G/Program.cs
@@ -119,8 +119,8 @@ class Program
              */
             //*****************************************************************************************************************************************
             //*****************************************************************************************************************************************
-            Console.WriteLine("Aschyn call of VER FILES scan");
-            Task.Run(() => VErfileScan());
+            Console.WriteLine(" call of VER FILES scan");
+            VErfileScan();
             //*****************************************************************************************************************************************
             //big buffer table 
             DataTable BigBuffer = MakeVerFileBufferTable();
@@ -135,9 +135,12 @@ class Program
 
                 if (Buffer.Count() == 0) 
                 {
-                    Console.Write("\r System ready (buffer empty)                           "); 
+                    Console.Write("\r System ready (buffer empty)   Rows in table: {0}                        ", BigBuffer.Rows.Count); 
                     spin.Turn();
-          
+                    BigBuffer.ExportToExcel(AppDomain.CurrentDomain.BaseDirectory + "VerFile.xlsx");
+                    Console.WriteLine("Done....");
+                    Console.ReadKey();
+
                 }
                 else
                 {
@@ -154,11 +157,13 @@ class Program
                             { 
                                 if (IsC3GVer(file)) 
                                 {
-                                    BigBuffer = ReadC3GVErFile(file);
-                                    Buffer.Delete(file);
-                                    Console.WriteLine(file);
+                          
+                                    foreach (DataRow dr in ReadC3GVErFile(file).Rows) 
+                                            {
+                                                BigBuffer.Rows.Add(dr.ItemArray);
+                                            }
                                     ReadC3GVErFile(file).WriteToCsvFile(@"c:\temp\test.csv");
-            
+                                    Buffer.Delete(file);
 
                                 } 
                                 else 
@@ -1426,4 +1431,66 @@ public static class DataTableExtensions {
         }
     }
 
+public static class My_DataTable_Extensions
+{
+
+    // Export DataTable into an excel file with field names in the header line
+    // - Save excel file without ever making it visible if filepath is given
+    // - Don't save excel file, just make it visible if no filepath is given
+    public static void ExportToExcel(this DataTable Tbl, string ExcelFilePath = null)
+    {
+        try
+        {
+            if (Tbl == null || Tbl.Columns.Count == 0)
+                throw new Exception("ExportToExcel: Null or empty input table!\n");
+
+            // load excel, and create a new workbook
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Workbooks.Add();
+
+            // single worksheet
+            Excel._Worksheet workSheet = excelApp.ActiveSheet;
+
+            // column headings
+            for (int i = 0; i < Tbl.Columns.Count; i++)
+            {
+                workSheet.Cells[1, (i + 1)] = Tbl.Columns[i].ColumnName;
+            }
+
+            // rows
+            for (int i = 0; i < Tbl.Rows.Count; i++)
+            {
+                // to do: format datetime values before printing
+                for (int j = 0; j < Tbl.Columns.Count; j++)
+                {
+                    workSheet.Cells[(i + 2), (j + 1)] = Tbl.Rows[i][j];
+                }
+            }
+
+            // check fielpath
+            if (ExcelFilePath != null && ExcelFilePath != "")
+            {
+                try
+                {
+                    workSheet.SaveAs(ExcelFilePath);
+                    excelApp.Quit();
+                   // MessageBox.Show("Excel file saved!");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("ExportToExcel: Excel file could not be saved! Check filepath.\n"
+                        + ex.Message);
+                }
+            }
+            else    // no filepath is given
+            {
+                excelApp.Visible = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("ExportToExcel: \n" + ex.Message);
+        }
+    }
+}
 } // end of namespace
